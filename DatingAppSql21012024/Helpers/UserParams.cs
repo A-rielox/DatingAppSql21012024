5be442c177b311cc0b0acc3f472401e585f75722
A-rielox <arielox.ag@gmail.com>
2024-02-16 17:04:36 -0600

    Listas clases para paginacion

---
+++ DatingAppSql21012024/Helpers/UserParams.cs
@@ -0,0 +1,11 @@
+﻿namespace DatingAppSql21012024.Helpers;
+
+public class UserParams : PaginationParams
+{
+    // este no me los envia, yo lo pongo en el controller
+    public string CurrentUsername { get; set; }
+    public string Gender { get; set; }
+    public int MinAge { get; set; } = 18;
+    public int MaxAge { get; set; } = 100;
+    public string OrderBy { get; set; } = "lastActive";
+}
