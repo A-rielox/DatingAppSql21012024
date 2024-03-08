7e7316213b1dc2b29de0b1ab8d551ff9197978ec
A-rielox <arielox.ag@gmail.com>
2024-03-08 12:47:24 -0600

    listo directiva de roles

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -25,6 +25,8 @@ import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { TextInputComponent } from './_forms/text-input/text-input.component';
 import { DatePickerComponent } from './_forms/date-picker/date-picker.component';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { HasRoleDirective } from './_directives/has-role.directive';
 // import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 
 @NgModule({
@@ -45,6 +47,8 @@ import { DatePickerComponent } from './_forms/date-picker/date-picker.component'
       PhotoEditorComponent,
       TextInputComponent,
       DatePickerComponent,
+      AdminPanelComponent,
+      HasRoleDirective,
       // MemberMessagesComponent,   ahora es Stand Alone Component
    ],
    imports: [
